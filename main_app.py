c522fd8d638a207a23681bc35cdfeaf93afe6f2c
PankajNk <68824616+PankajNk@users.noreply.github.com>
2020-12-02 22:45:05 +0530

    Add files via upload

---
+++ main_app.py
@@ -0,0 +1,26 @@
+from flask import Flask ,request,render_template
+import pickle 
+import numpy as np
+
+app = Flask(__name__)
+loaded_model = pickle.load(open("Weight_Height_Model.pkl","rb"))
+
+@app.route("/")
+def home():
+	return render_template("index.html")
+
+@app.route("/getpredication",methods=['POST'] )
+def getpredication():
+	input = [float(x) for x in request.form.values()]
+	final_input = [np.array(input)]
+	print(f"final_input")
+	predication = loaded_model.predict(final_input)
+	
+
+	return render_template("index.html",output = "Predicated weight {}".format(predication))
+
+
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
